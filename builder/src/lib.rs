c0b21e6ba4cb3e3aaec411b887bf0414750c7afb
importcjj <importcjj@gmail.com>
2020-05-11 23:24:45 +0800

    Pass builder/06-optional-field

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -4,15 +4,15 @@ use proc_macro::TokenStream;
 
 use proc_macro2::Span;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Fields, Ident};
+use syn::{parse_macro_input, Data, DeriveInput, Fields, Ident, Path, Type, TypePath};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = input.ident;
 
-    let struct_name = generate_builder_name(&name);
-    let builder = generate_builder_struct(&struct_name, &input.data);
+    let builder_name = generate_builder_name(&name);
+    let builder = generate_builder_struct(&name, &builder_name, &input.data);
 
     let expanded = quote! {
         #builder
@@ -26,40 +26,113 @@ fn generate_builder_name(name: &Ident) -> Ident {
     Ident::new(&builder_name, Span::call_site())
 }
 
-fn generate_builder_struct(struct_name: &Ident, data: &Data) -> proc_macro2::TokenStream {
+fn generate_builder_struct(
+    struct_name: &Ident,
+    builder_name: &Ident,
+    data: &Data,
+) -> proc_macro2::TokenStream {
     match *data {
         Data::Struct(ref data) => match data.fields {
             Fields::Named(ref fields) => {
+                let names_tys = fields.named.iter().map(|f| {
+                    let name = f.ident.as_ref().unwrap();
+                    let ty = match parse_inner_ty("Option", &f.ty) {
+                        Some(inner) => inner,
+                        None => &f.ty
+                    };
+                    quote! {#name: Option<#ty>}
+                });
+
                 let names = fields.named.iter().map(|f| {
                     let name = f.ident.as_ref().unwrap();
                     quote! { #name }
                 });
 
-                let names2 = names.clone();
+                let setter_functions = fields.named.iter().map(|f| {
+                    let name = f.ident.as_ref().unwrap();
+                    let ty = match parse_inner_ty("Option", &f.ty) {
+                        Some(inner) => inner,
+                        None => &f.ty,
+                    };
 
-                let tys = fields.named.iter().map(|f| {
-                    let ty = &f.ty;
-                    quote! { Option<#ty> }
+                    quote! {
+                        pub fn #name(&mut self, #name: #ty) -> &mut Self {
+                            self.#name = Some(#name);
+                            self
+                        }
+                    }
+                });
+
+                let set_fields_values = fields.named.iter().map(|f| {
+                    let name = f.ident.as_ref().unwrap();
+
+                    match parse_inner_ty("Option", &f.ty) {
+                        Some(_) => {
+                            quote! {
+                                #name: self.#name.take()
+                            }
+                        }
+                        None => {
+                            let err = format!("field {} missed", name);
+                            quote! {
+                                #name: self.#name.take().ok_or_else(|| {
+                                    let e: Box<dyn std::error::Error> = #err.into();
+                                    e
+                                })?
+                            }
+                        }
+                    }
                 });
 
                 quote! {
-                    pub struct #struct_name {
-                        #(#names2: #tys,)*
-                    }
-
-
                     impl #struct_name {
-                        fn builder() -> #struct_name {
-                            #struct_name {
+                        pub fn builder() -> #builder_name {
+                            #builder_name {
                                 #(#names: None,)*
                             }
                         }
                     }
+
+                    pub struct #builder_name {
+                        #(#names_tys,)*
+                    }
+
+                    impl #builder_name {
+                        #(#setter_functions)*
+
+
+                        pub fn build(&mut self) -> Result<#struct_name, Box<dyn std::error::Error>> {
+                            std::result::Result::Ok(#struct_name{
+                                #(#set_fields_values,)*
+                            })
+                        }
+                    }
                 }
             }
-            Fields::Unnamed(ref fields) => unimplemented!(),
+            Fields::Unnamed(..) => unimplemented!(),
             Fields::Unit => unimplemented!(),
         },
         Data::Enum(_) | Data::Union(_) => unimplemented!(),
     }
+}
+
+fn parse_inner_ty<'a>(outer: &str, ty: &'a Type) -> Option<&'a Type> {
+    if let Type::Path(TypePath {
+        qself: None,
+        path: Path { segments, .. },
+    }) = ty
+    {
+        let first_seg = &segments[0];
+        if first_seg.ident != outer {
+            return None;
+        }
+
+        if let syn::PathArguments::AngleBracketed(ref args) = first_seg.arguments {
+            if let syn::GenericArgument::Type(ref ty) = &args.args[0] {
+                return Some(ty);
+            }
+        }
+    }
+
+    None
 }
