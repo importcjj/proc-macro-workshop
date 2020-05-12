679d5df218fa29ef2b50e7c13e10ec31dca198d8
importcjj <importcjj@gmail.com>
2020-05-12 23:59:14 +0800

    Pass builder/07

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -4,9 +4,13 @@ use proc_macro::TokenStream;
 
 use proc_macro2::Span;
 use quote::quote;
-use syn::{parse_macro_input, Data, DeriveInput, Fields, Ident, Path, Type, TypePath};
+use syn::parse::ParseStream;
+use syn::{
+    parse_macro_input, Attribute, Data, DeriveInput, Fields, Ident, LitStr, Path, Result, Token,
+    Type, TypePath,
+};
 
-#[proc_macro_derive(Builder)]
+#[proc_macro_derive(Builder, attributes(builder))]
 pub fn derive(input: TokenStream) -> TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     let name = input.ident;
@@ -36,9 +40,9 @@ fn generate_builder_struct(
             Fields::Named(ref fields) => {
                 let names_tys = fields.named.iter().map(|f| {
                     let name = f.ident.as_ref().unwrap();
-                    let ty = match parse_inner_ty("Option", &f.ty) {
+                    let ty = match get_inner_ty("Option", &f.ty) {
                         Some(inner) => inner,
-                        None => &f.ty
+                        None => &f.ty,
                     };
                     quote! {#name: Option<#ty>}
                 });
@@ -50,15 +54,33 @@ fn generate_builder_struct(
 
                 let setter_functions = fields.named.iter().map(|f| {
                     let name = f.ident.as_ref().unwrap();
-                    let ty = match parse_inner_ty("Option", &f.ty) {
+                    let ty = match get_inner_ty("Option", &f.ty) {
                         Some(inner) => inner,
                         None => &f.ty,
                     };
 
-                    quote! {
-                        pub fn #name(&mut self, #name: #ty) -> &mut Self {
-                            self.#name = Some(#name);
-                            self
+                    let each_attr = get_each_attr(&f.attrs).unwrap();
+                    match each_attr.name {
+                        Some(each_name) => {
+                            let vec_ty = get_inner_ty("Vec", &ty).unwrap();
+
+                            quote! {
+                                pub fn #each_name(&mut self, #each_name: #vec_ty) -> &mut Self {
+                                    match self.#name {
+                                        Some(ref mut v) => v.push(#each_name),
+                                        None => self.#name = Some(vec![#each_name])
+                                    }
+                                    self
+                                }
+                            }
+                        }
+                        None => {
+                            quote! {
+                                pub fn #name(&mut self, #name: #ty) -> &mut Self {
+                                    self.#name = Some(#name);
+                                    self
+                                }
+                            }
                         }
                     }
                 });
@@ -66,19 +88,29 @@ fn generate_builder_struct(
                 let set_fields_values = fields.named.iter().map(|f| {
                     let name = f.ident.as_ref().unwrap();
 
-                    match parse_inner_ty("Option", &f.ty) {
+                    match get_inner_ty("Option", &f.ty) {
                         Some(_) => {
                             quote! {
                                 #name: self.#name.take()
                             }
                         }
                         None => {
-                            let err = format!("field {} missed", name);
-                            quote! {
-                                #name: self.#name.take().ok_or_else(|| {
-                                    let e: Box<dyn std::error::Error> = #err.into();
-                                    e
-                                })?
+                            let each_attr = get_each_attr(&f.attrs).unwrap();
+                            let is_vec = each_attr.name.is_some();
+                            if is_vec {
+                                quote! {
+                                    #name: self.#name.take().unwrap_or_else(|| {
+                                        vec![]
+                                    })
+                                }
+                            } else {
+                                let err = format!("field {} missed", name);
+                                quote! {
+                                    #name: self.#name.take().ok_or_else(|| {
+                                        let e: Box<dyn std::error::Error> = #err.into();
+                                        e
+                                    })?
+                                }
                             }
                         }
                     }
@@ -116,7 +148,7 @@ fn generate_builder_struct(
     }
 }
 
-fn parse_inner_ty<'a>(outer: &str, ty: &'a Type) -> Option<&'a Type> {
+fn get_inner_ty<'a>(outer: &str, ty: &'a Type) -> Option<&'a Type> {
     if let Type::Path(TypePath {
         qself: None,
         path: Path { segments, .. },
@@ -135,4 +167,29 @@ fn parse_inner_ty<'a>(outer: &str, ty: &'a Type) -> Option<&'a Type> {
     }
 
     None
+}
+
+struct EachAttr {
+    name: Option<Ident>,
+}
+
+fn get_each_attr(input: &[Attribute]) -> Result<EachAttr> {
+    let mut each_attr = EachAttr { name: None };
+
+    syn::custom_keyword!(each);
+
+    for attr in input {
+        if attr.path.is_ident("builder") {
+            attr.parse_args_with(|input: ParseStream| {
+                input.parse::<Option<each>>()?;
+                input.parse::<Token![=]>()?;
+                let lit = input.parse::<LitStr>()?;
+                let name = Ident::new(&lit.value(), lit.span());
+                each_attr.name = Some(name);
+                Ok(())
+            })?;
+        }
+    }
+
+    Ok(each_attr)
 }
